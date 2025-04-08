ed4db8cb08749bac86aed1edffa239c02b0bd1d1
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-08 15:21:37 +1000

    Тесты начаты, но не работает

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from materials.models import Lesson, Course, Subscribe
+from users.models import CustomUser
+
+
+class MaterialsTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create(username='test', email='test@yandex.ru', password='1234qwerty')
+        self.course = Course.objects.create(name='1 Course',
+                                            description='First course',
+                                            owner=self.user)
+        self.lesson = Lesson.objects.create(name='Lesson 1',
+                                            course=self.course,
+                                            description='This is lesson 1',
+                                            video_link='http://youtube.com')
+        self.subscribe = Subscribe(user=self.user, course=self.course)
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse('materials:lesson-detail', args=(self.lesson.pk, ))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get('name'), self.lesson.name)
+
+
+
--- materials/urls.py
+++ materials/urls.py
@@ -17,11 +17,11 @@ router.register(r"course", CourseViewSet, basename="course")
 app_name = MaterialsConfig.name
 
 urlpatterns = [
-    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
-    path("lesson/", LessonListAPIView.as_view(), name="lesson_list"),
-    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
-    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson_update"),
-    path("lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson_delete"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lesson/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-detail"),
+    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson-update"),
+    path("lesson/delete/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson-delete"),
     path("course/subscribe", CourseSubscribeApiView.as_view(), name="subscribe"),
 ]
 
