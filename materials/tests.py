64086604f964abbb5d78376f3e0efac921e953c8
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-11 09:45:53 +1000

    Тесты исправны

--- materials/tests.py
+++ materials/tests.py
@@ -84,3 +84,10 @@ class SubscribeTestCase(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(data["message"], "подписка добавлена")
+
+        data = {"course":self.course.pk}
+        response = (self.client.post(url, data))
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data["message"], "подписка удалена")
