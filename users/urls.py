fd29c51f14146a4f1862a9f4167a3b7a26fefff1
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-15 16:11:41 +1000

    Прописана оплата, swagger

--- users/urls.py
+++ users/urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
 from rest_framework.permissions import AllowAny
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import (CustomUserCreateAPIView, PaymentsViewSet, CustomUserListAPIView,
+from users.views import (CustomUserCreateAPIView, PaymentsCreateAPIView, CustomUserListAPIView,
                          CustomUserRetrieveAPIView, CustomUserDestroyAPIView, CustomUserUpdateAPIView)
 
-router = SimpleRouter()
-
-
-router.register(r"payments", PaymentsViewSet, basename="payments")
-#router.register(r"users", CustomUserListAPIView, basename="users")
 
 app_name = UsersConfig.name
+
+router = DefaultRouter()
 
 urlpatterns = [
     path("register/", CustomUserCreateAPIView.as_view(), name="register"),
@@ -23,6 +20,7 @@ urlpatterns = [
     path("users/<int:pk>/", CustomUserRetrieveAPIView.as_view(), name="user_detail"),
     path("users/<int:pk>/delete", CustomUserDestroyAPIView.as_view(), name="user_delete"),
     path("users/<int:pk>/update", CustomUserUpdateAPIView.as_view(), name="user_update"),
+    path('pay/course/<int:course_id>/', PaymentsCreateAPIView.as_view(), name='pay_course')
 ]
 
 urlpatterns += router.urls
